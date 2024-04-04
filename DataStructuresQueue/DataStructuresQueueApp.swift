120976d7172df5652cf3fde687a8bc5d858aa40d
shardakova svetlana <shardakova.s@medlinx.online>
2024-04-04 16:46:01 +0200

    Initial Commit

---
+++ DataStructuresQueue/DataStructuresQueueApp.swift
@@ -0,0 +1,17 @@
+//
+//  DataStructuresQueueApp.swift
+//  DataStructuresQueue
+//
+//  Created by Svetlana Shardakova on 04/04/24.
+//
+
+import SwiftUI
+
+@main
+struct DataStructuresQueueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
